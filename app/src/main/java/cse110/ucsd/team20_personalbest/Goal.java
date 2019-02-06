813e0a3e9c27d492e1942f49bd552a11746bdf5f
Yiming Wang <yiw679@ucsd.edu>
2019-02-06 12:30:35 -0800

    Goal class added

---
+++ app/src/main/java/cse110/ucsd/team20_personalbest/Goal.java
@@ -0,0 +1,17 @@
+package cse110.ucsd.team20_personalbest;
+
+public class Goal {
+    private int goal;
+
+    public Goal() {
+        goal = 5000;
+    }
+
+    public int getGoal() {
+        return goal;
+    }
+
+    public void setGoal(int val) {
+        goal = val;
+    }
+}
