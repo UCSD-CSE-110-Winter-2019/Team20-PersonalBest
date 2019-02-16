fb1fe7e7571c7cfb1c3b618c23ef8832f18a3771
Jeremy Phan Nguyen <jphanngu@ucsd.edu>
2019-02-16 12:48:43 -0800

    5.) Added preliminary test for pressing the start stop button

--- app/src/androidTest/java/cse110/ucsd/team20_personalbest/TrackStepsUnitTest.java
+++ app/src/androidTest/java/cse110/ucsd/team20_personalbest/TrackStepsUnitTest.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.junit.Rule;
+import android.support.test.rule.ActivityTestRule;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.RuntimeEnvironment;
+//import org.robolectric.Robolectric;
+//import org.robolectric.RobolectricTestRunner;
+//import org.robolectric.RuntimeEnvironment;
 
 import cse110.ucsd.team20_personalbest.fitness.FitnessService;
 import cse110.ucsd.team20_personalbest.fitness.FitnessServiceFactory;
@@ -24,9 +27,11 @@ public class TrackStepsUnitTest {
     private static final String TEST_SERVICE = "TEST_SERVICE";
     private String fitnessServiceKey = "GOOGLE_FIT";
 
-    private MainActivity activity;
+    //private MainActivity activity;
     private TextView textViewSteps;
     private long nextStepCount;
+
+    public ActivityTestRule<MainActivity> activity = new ActivityTestRule<MainActivity>(MainActivity.class);
 
     @Before
     public void setUp() throws Exception {
@@ -41,20 +46,20 @@ public class TrackStepsUnitTest {
 
         });
 
-        Intent intent = new Intent(RuntimeEnvironment.application, MainActivity.class);
-        intent.putExtra(fitnessServiceKey, TEST_SERVICE);
+        //Intent intent = new Intent(RuntimeEnvironment.application, MainActivity.class);
+        //intent.putExtra(fitnessServiceKey, TEST_SERVICE);
         //activity = Robolectric.buildActivity(MainActivity.class, intent).create().get();
 
 
-        textViewSteps = activity.findViewById(R.id.textViewSteps);
+        textViewSteps = activity.getActivity().findViewById(R.id.textViewSteps);
         nextStepCount = 1337;
 
     }
 
-    @Test
-    public void testInitialSteps() {
-        assertEquals("Steps", textViewSteps.getText().toString());
-    }
+    //@Test
+    //public void testInitialSteps() {
+       // assertEquals("Steps", textViewSteps.getText().toString());
+    //}
 
     private class TestFitnessService implements FitnessService {
 
