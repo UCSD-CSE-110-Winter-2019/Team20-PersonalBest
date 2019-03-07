75135b1d1ee06b7cea0942e20a58ad5cd2a4838d
DominicSimone <dominic.simone@gmail.com>
2019-03-07 15:06:04 -0800

    #97 Adding activity to view friends history

--- app/src/main/java/cse110/ucsd/team20_personalbest/GraphMessageActivity.java
+++ app/src/main/java/cse110/ucsd/team20_personalbest/GraphMessageActivity.java
@@ -9,5 +9,7 @@ public class GraphMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_message);
+
+
     }
 }
